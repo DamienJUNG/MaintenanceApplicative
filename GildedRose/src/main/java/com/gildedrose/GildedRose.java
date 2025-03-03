da6bd01846df21f20c61a98cfa58fd9b7f6392eb
Damien Jung <jungdamien02@gmail.com>
2025-03-03 09:38:24 +0100

    Refacto updateQuality

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,54 +12,67 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            handleItem(items[i]);
+        }
+    }
+    
+    public void handleItem(Item item) {
+        switch (item.name) {
+            case AGED_BRIE -> handleBrie(item);
+            case TAFKAL_80_ETC_CONCERT -> handleTaflkalPasses(item);
+            case SULFURAS -> handleSulfuras(item);
+            default -> handleGenericItem(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    private void handleGenericItem(Item item) {
+        item.sellIn = item.sellIn - 1;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        if(item.quality > 0){
+            item.quality = item.quality - 1;
+            if(item.quality > 0 && item.sellIn < 0) {
+                item.quality = item.quality - 1;
             }
         }
+    }
+
+    private void handleSulfuras(Item item) {
+        //Sulfuras is a legendary item
+    }
+
+    private void handleTaflkalPasses(Item item) {
+        item.sellIn = item.sellIn - 1;
+
+        if(item.quality < 50){
+            if(item.sellIn < 0){
+                item.quality = 0;
+            }
+            else if(item.sellIn < 6){
+                item.quality = item.quality + 3;
+            }
+            else if(item.sellIn < 11){
+                item.quality = item.quality + 2;
+            }
+            else {
+                item.quality += 1;
+            }
+
+            if(item.quality > 50){
+                item.quality = 50;
+            }
+        }
+    }
+
+    private void handleBrie(Item item) {
+        item.sellIn = item.sellIn - 1;
+
+        if(item.quality < 50){
+            item.quality = item.quality + 1;
+
+            if(item.quality < 50 && item.sellIn < 0){
+                item.quality = item.quality + 1;
+            }
+        }
+
     }
 }
--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,72 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    @Nested
+    class forAnyValidItem {
+        @ParameterizedTest
+        @MethodSource("itemProvider")
+        public void updateShouldBeCorrect(Item item, int sellIn, int quality) {
+            GildedRose app = new GildedRose(new Item[]{item});
+            app.updateQuality();
+
+            assertEquals(sellIn, app.items[0].sellIn);
+            assertEquals(quality, app.items[0].quality);
+        }
+
+        public static Stream<Arguments> itemProvider(){
+            return Stream.of(
+                    //Base test
+                    Arguments.of(new Item("Aged Brie", 20,20), 19,21),
+                    Arguments.of(new Item("Sulfuras, Hand of Ragnaros", 10, 80), 10, 80),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 100, 5), 99, 6),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 5), 9, 7),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5), 4, 8),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5), -1, 0),
+                    Arguments.of(new Item("Patate", 10, 5), 9, 4),
+
+                    //High Degradation
+                    Arguments.of(new Item("Aged Brie", 0,20), -1,22),
+                    Arguments.of(new Item("Sulfuras, Hand of Ragnaros", 0, 80), 0, 80),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5), -1, 0),
+                    Arguments.of(new Item("Patate", 0, 5), -1, 3),
+
+                    //Null Quality
+                    Arguments.of(new Item("Aged Brie", 10,0), 9,1),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0), 9, 2),
+                    Arguments.of(new Item("Patate", 10, 0), 9, 0),
+
+                    //Max quality
+                    Arguments.of(new Item("Aged Brie", 10,50), 9,50),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50), 14, 50),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50), 9, 50),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50), 3, 50),
+                    Arguments.of(new Item("Patate", 10, 50), 9, 49),
+
+                    //Reaching Max Quality
+                    Arguments.of(new Item("Aged Brie", 10,49), 9,50),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 49), 14, 50),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49), 9, 50),
+                    Arguments.of(new Item("Backstage passes to a TAFKAL80ETC concert", 4, 49), 3, 50),
+
+                    //Rotted potatoes && sulfuras &&
+                    Arguments.of(new Item("Patate", 0, 50), -1, 48),
+                    Arguments.of(new Item("Patate", 0, 1), -1, 0),
+                    Arguments.of(new Item("Patate", 0, 0), -1, 0),
+                    Arguments.of(new Item("Sulfuras, Hand of Ragnaros", -1, 80), -1, 80),
+                    Arguments.of(new Item("Aged Brie", 0,50), -1,50)
+                    );
+        }
     }
 
 }
